27405cae2f9945648f9d1597454eb46791e56574
mdanikibun <md.anikibun@gmail.com>
2024-09-05 10:03:08 +0700

    feat(cronjob): build cronjob auto icrease mochi each 2 hours from 8h to 18h

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('mochi:increase')
+            ->everyTwoHours()
+            ->between('8:00', '18:00');
     }
 
     /**
