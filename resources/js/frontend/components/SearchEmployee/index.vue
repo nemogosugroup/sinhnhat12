4caf561f9d1badcbc1e8ef36d7f9f6759bcef8dc
DESKTOP-OR12Q9F\Admin <phetranduy@gmail.com>
2024-09-05 16:22:40 +0700

    update layout and filter employee moonwalk

--- resources/js/frontend/components/SearchEmployee/index.vue
+++ resources/js/frontend/components/SearchEmployee/index.vue
@@ -2,9 +2,19 @@
     <div class="search">
         <h6 class="title-h6"> Nguyệt đa thần trụ</h6>
         <div>
-            <el-select v-model="value" filterable clearable remote reserve-keyword placeholder="Tìm kiếm nhân sự"
-                :remote-method="remoteMethod" :loading="loading" @change="handleChange">
-                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+            <el-select ref="employeeSelect" v-model="selectedEmployee" filterable clearable remote reserve-keyword
+                placeholder="Tìm kiếm nhân sự" :remote-method="remoteMethod" :loading="loading" @change="handleChange">
+                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+                    <span class="wrap-images">
+                        <span class="images">
+                            <img :src="item.avatar" alt="">
+                        </span>
+                        <span class="info">
+                            <strong>{{ item.label }}</strong>
+                            <span class="job">{{ item.job }}</span>
+                        </span>
+                    </span>
+                </el-option>
             </el-select>
         </div>
     </div>
@@ -13,27 +23,42 @@
 import { Employee } from './data';
 export default {
     name: "SearchEmployee",
+    props: {
+        nameEmployee: {
+            type: String,
+            default: null
+        }
+    },
     data() {
         return {
             list: [],
             options: [],
             value: null,
             loading: false,
+            selectedEmployee: this.nameEmployee
         };
+    },
+    watch: {
+        nameEmployee(newValue) {
+            this.selectedEmployee = newValue; // Cập nhật khi có thay đổi từ parent
+        },
+        selectedEmployee(newValue) {
+            this.$emit('update:name-employee', newValue); // Truyền giá trị về parent khi el-select thay đổi
+        }
     },
     mounted() {
         this.list = Employee.map((item) => {
-            return { value: item, label: item };
+            return { value: item.hoten, label: item.hoten, avatar: item.avatar, job: item.trungtam };
         });
     },
     methods: {
-        remoteMethod(query) {
-            if (query) {
+        remoteMethod() {
+            if (this.selectedEmployee) {
                 this.loading = true;
                 setTimeout(() => {
                     this.loading = false;
                     this.options = this.list.filter((item) => {
-                        return item.label.toLowerCase().includes(query.toLowerCase());
+                        return item.label.toLowerCase().includes(this.selectedEmployee.toLowerCase());
                     });
                 }, 200);
             } else {
@@ -42,7 +67,13 @@ export default {
         },
         handleChange() {
             console.log(this.value)
-        }
+        },
+        focusSelect() {
+            this.$refs.employeeSelect.focus();
+            this.$nextTick(() => {
+                this.$refs.employeeSelect.toggleMenu(); // Mở menu dropdown
+            });
+        },
     },
 };
 </script>
@@ -61,17 +92,40 @@ export default {
 }
 
 .el-select-dropdown__item {
-    color: #000;
-    position: relative;
-    display: flex;
-    align-items: center;
+    height: 50px;
+    line-height: 20px;
 
-    &:before {
-        content: url(../../../assets/images/sinhnhat/icon_employee.png);
-        width: 20px;
-        height: 20px;
-        margin-right: 5px;
+    .wrap-images {
+        color: #000;
+        position: relative;
+        display: flex;
+        align-items: center;
+        font-weight: 700;
+
+        .images {
+            width: 30px;
+            height: 30px;
+            border-radius: 100%;
+            display: inline-flex;
+            margin-right: 3px;
+            overflow: hidden;
+
+            img {
+                width: 100%;
+                height: 100%;
+            }
+        }
+
+        strong {
+            font-weight: 700;
+        }
+
+        .job {
+            font-weight: 400;
+            display: block;
+        }
     }
+
 }
 
 // el-select-dropdown__item is-hovering
